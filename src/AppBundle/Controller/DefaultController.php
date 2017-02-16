c3f986be8d8f11ad5703ef668c1dc5ee77f169a4
Jujhar <jujhar.pannu@gmail.com>
2017-02-15 16:15:27 -0800

    updated module render in controller and module display logic in views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class DefaultController extends Controller
 {
     /**
@@ -42,9 +43,53 @@ class DefaultController extends Controller
         $repository = $this->getDoctrine()->getRepository('AppBundle:Item');
         $item = $repository->findOneByName($item_name);
 
+        // Render item
+        $module = DefaultController::preRenderModule($item_name);
+
         return $this->render('default/item.html.twig', [
             'item' => $item,
-            'view' => 'items/' . mb_strtolower($item->name) . '.php'
+            'view' => 'items/' . mb_strtolower($item->name) . '.html.twig',
+            'module' => $module
         ]);
+    }
+
+    public function preRenderModule($name) {
+
+        if($name == 'Faker') {
+
+
+            $faker = \Faker\Factory::create();
+
+            // generate data by accessing properties
+            $fname = $faker->name;
+            // 'Lucy Cechtelar';
+            $faddress = $faker->address;
+            // "426 Jordy Lodge
+            // Cartwrightshire, SC 88120-6700"
+            $ftext = $faker->text;
+
+            $returnme = (object)[
+                'name' => $fname,
+                'address' => $faddress,
+                'text' => $ftext,
+            ];
+
+            return $returnme;
+
+        }
+
+        //if (name == 'Imagine') {
+            /*
+             todo
+            $palette = new Imagine\Image\Palette\RGB();
+
+            $image = $imagine->create(new Box(400, 300), $palette->color('#000'));
+
+            $image->draw()->ellipse(new Point(200, 150), new Box(300, 225), $image->palette()->color('fff'));
+
+            $image->save('ellipse.png');
+
+             */
+        //}
     }
 }
